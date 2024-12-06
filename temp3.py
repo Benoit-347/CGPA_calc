ca8c906e20cd369ad767aaccaf3febf91dc48595
Benoit-347 <benoitkuriakose@gmail.com>
2024-12-06 22:18:12 +0530

    beta 1

--- temp3.py
+++ temp3.py
@@ -1,6 +1,2 @@
-#the dict: {'math': {'ia': [["value:", value], [9, 10],[8,10]], 'lab':}}
-for type_name in types:
-    row.append(type_name)
-    type_list = types[type_name]
-    for i in type_list:
-        row.ext
+a = [1,2,3]
+print(int(""))
